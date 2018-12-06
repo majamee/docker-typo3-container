01a4745655b87854ab4b862d484269932186375a
majamee <majamee@yandex.com>
2018-12-06 17:57:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,3 @@ trap : TERM INT
 echo -e "\nSetting permissions for all created files and folders & finishing";
 cd /var/www/html && chown -R www-data . ;
 echo -e "\nReady to connect to http://localhost";
-
-# Keep container running
-tail -f /dev/null & wait
