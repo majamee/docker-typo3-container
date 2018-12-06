2fdd42d16b70365aebb2127501fe09b5c1fcd89c
majamee <majamee@yandex.com>
2018-12-06 17:24:30 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,3 @@
 echo -e "\nSetting permissions for all created files and folders & finishing";
 cd /var/www/html && chown -R www-data . ;
 echo -e "\nReady to connect to http://localhost";
-
-# Keep Docker container running after permissions are fixed for Linux
-tail -f /bin/entrypoint.sh;
