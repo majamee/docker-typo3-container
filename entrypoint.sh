342d616e5ba12020df7adfeb8bcc73e35d6129fe
majamee <majamee@yandex.com>
2018-12-06 17:02:41 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,5 @@
 echo -e "\nSetting permissions for all created files and folders & finishing";
 cd /var/www/html && chown -R www-data . ;
 echo -e "\nReady to connect to http://localhost";
+
+tail -f /bin/entrypoint.sh
