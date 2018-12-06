7ebe6007f3d70387a9169c8668b2bbfbd424332a
majamee <majamee@yandex.com>
2018-12-06 17:42:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+trap : TERM INT
+
 # Fix permissions for Linux
 echo -e "\nSetting permissions for all created files and folders & finishing";
 cd /var/www/html && chown -R www-data . ;
 echo -e "\nReady to connect to http://localhost";
+
+# Keep container running
+tail -f /dev/null & wait
