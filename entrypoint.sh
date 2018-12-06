67ea8711e4af7b2350f02e467e223f4b7a0df0d4
majamee <majamee@yandex.com>
2018-12-06 15:34:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,7 @@
 # Fix permissions for Linux
 echo -e "\nSetting permissions for all created files and folders & finishing";
 cd /var/www/html && chown -R www-data .
+echo -e "\nReady to connect by opening http://localhost";
+
+#Extra line added in the script to run all command line arguments
+exec "$@";
