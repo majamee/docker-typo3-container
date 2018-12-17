f6345fd8e4d024406f243eea90c048a6a7a8d651
majamee <majamee@yandex.com>
2018-12-17 10:29:56 +0100

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Fix permissions for Linux
-echo -e "\nSetting permissions for all created files and folders & finishing";
-cd /var/www/html && chown -R www-data . ;
-echo -e "\nReady to connect to http://localhost";
